383b8106a5de6642f77636aa81db581d46735340
EL-omg <refaeltryfernandoo@gmail.com>
2026-01-19 01:36:29 +0700

    Add files via upload
    // Top-level build file where you can add configuration options common to all sub-projects/modules.
    
    plugins {
        id("com.android.application") version "8.1.0" apply false
        // HAPUS baris ini jika pakai Java (bukan Kotlin):
        // id("org.jetbrains.kotlin.android") version "1.9.0" apply false
    }

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    // HAPUS baris ini jika pakai Java (bukan Kotlin):
+    // id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
